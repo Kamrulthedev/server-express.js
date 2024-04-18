c79d500be7e408165bc44aeb447f3256baf406a5
Kamrul Hassan <kamrulthedev@gmail.com>
2024-04-19 05:57:28 +0600

    medal-waer-commit

--- server-express.js
+++ server-express.js
@@ -1,13 +1,47 @@
-
 const express = require('express');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+require('dotenv').config();
 
 
-app.get('/home', (req, res) => {
-    res.sendFile(__dirname+ '/index.html')
-  });
+app.use(express.json());
 
-  app.listen(5000, () => {
-    console.log(`Example app listening on port 5000`)
-  });
+
+const uri = "mongodb+srv://emaJohn:lgjeSOws84nPIQFC@cluster0.kqeap4x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    app.get('/home', (req, res) => {
+      res.sendFile(__dirname + '/index.html')
+    });
+
+    app.get('/create-post', (req, res) => {
+          
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(5000, () => {
+  console.log(`Example app listening on port 5000`)
+});
